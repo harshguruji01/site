4687c4a880fd9efe4fa3d85acb7da97f60fedc0d
harshguruji01 <harshguruji01@gmail.com>
2026-09-11 16:23:57 +0530

    chore(supabase): update Supabase publishable key and dependencies

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 const supabaseUrl = 'https://wumdbpyhpblvgjttsbpv.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1bWRicHlocGJsdmdqdHRzYnB2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYyNDMwNDQsImV4cCI6MjEwMTgxOTA0NH0.6eK0veaHyf6Dk9UMX6fGrxNItsfa4Y-m-NE6PXbCQ80';
+const supabaseKey = 'sb_publishable_xLqKY9N62MXb6ELG-5trig_RlJs_n-l';
 
 // Initialize Supabase
 export const supabase = createClient(supabaseUrl, supabaseKey);
